f235149b84f5652e45c3a9a6281c3f9c1bc6ef12
Cyan-zhou <jingyanzhou97@gmail.com>
2023-09-13 22:15:38 +0800

    2023/9/13/22:15

--- posts/path.py
+++ posts/path.py
@@ -1,4 +1,4 @@
 a = input("input the address：")
 
 
-print("<center><a data-fancybox=\"gallery\" href=\"%s\"><img src=\"%s\"></a><body></center>" % (a, a))
+print("<center><a data-fancybox=\"gallery\" href=\"%s\"><img src=\"%s\"></a></center>" % (a, a))
